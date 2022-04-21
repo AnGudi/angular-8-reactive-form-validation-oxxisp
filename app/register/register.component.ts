5ef5568f5bde9c6d8bb3f29efb0ed62a5fc8ff25
AnGudi <gudianjali0208@gmail.com>
2022-04-21 13:33:27 +0530

    3 files modified

--- app/register/register.component.ts
+++ app/register/register.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { EmployeeService } from '../services/employee.service';
 import { MustMatch } from '../_helpers/must-match.validator';
 
 // import custom validator to validate that password and confirm password fields match
@@ -11,11 +18,22 @@ import { MustMatch } from '../_helpers/must-match.validator';
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
+  id!: string;
+  isAddMode!: boolean;
+  loading = false;
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private employeeService: EmployeeService
+  ) {}
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.isAddMode = !this.id;
+
     this.registerForm = this.formBuilder.group(
       {
         title: ['', Validators.required],
@@ -38,6 +56,13 @@ export class RegisterComponent implements OnInit {
         validator: MustMatch('password', 'confirmPassword'),
       }
     );
+    
+    if (!this.isAddMode) {
+      this.employeeService
+        .getAll(this.id)
+        .pipe(first())
+        .subscribe((x) => this.registerForm.patchValue(x));
+    }
   }
 
   // convenience getter for easy access to form fields
@@ -55,7 +80,7 @@ export class RegisterComponent implements OnInit {
 
     // display form values on success
     alert(
-      'SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4)
+      'SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 5)
     );
   }
 
